8a5108e7913feef54188f07793f65a61cde44b3e
Christopher Tessum <ctessum@gmail.com>
2023-01-21 21:26:24 -0600

    Remove composition.jl

--- src/EarthSciMLBase.jl
+++ src/EarthSciMLBase.jl
@@ -2,7 +2,6 @@ module EarthSciMLBase
 using ModelingToolkit, Symbolics, Catalyst
 using DocStringExtensions
 
-include("composition.jl")
 include("add_dims.jl")
 include("icbc.jl")
 include("composed_system.jl")
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,6 @@ using EarthSciMLBase
 using Test, SafeTestsets
 
 @testset "EarthSciMLBase.jl" begin
-    @safetestset "Composition" begin include("composition_test.jl") end
     @safetestset "AddDims" begin include("add_dims_test.jl") end
     @safetestset "ICBC" begin include("icbc_test.jl") end
     @safetestset "Composed System" begin include("composed_system_test.jl") end
